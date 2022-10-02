3a8c9c59a20a87fe3711bd71e528e24d709f7c1c
Lukas D <lukas.dony@web.de>
2022-10-02 11:30:47 +0200

    Add dev tools
    Visual Studio, Git, GitExtensions, IntelliJ Toolbox, Winmerge

--- WindowsInstall/InstallAllTools.ps1
+++ WindowsInstall/InstallAllTools.ps1
@@ -1,5 +1,8 @@
 Write-Output "Installing communication tools..."
 & .\InstallCommunicationTools.ps1
 
+Write-Output "Installing dev tools..."
+& .\InstallDevTools.ps1
+
 Write-Output "All script executed."
 Pause
---
+++ WindowsInstall/InstallDevTools.ps1
@@ -0,0 +1,5 @@
+winget install --id=Microsoft.VisualStudio.2022.Community-Preview -e  ;
+winget install --id=WinMerge.WinMerge -e  ;
+winget install --id=GitExtensionsTeam.GitExtensions -e  ;
+winget install --id=Git.Git -e  ;
+winget install --id=JetBrains.Toolbox -e 
